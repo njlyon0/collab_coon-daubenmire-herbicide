46b14f80d385b5654380a0719ca5a199a5c8dec9
Nick J Lyon <nick.lyon@Nicks-MBP.lan>
2020-11-09 13:33:34 -0500

    Start of the code that will do the revisions in response to our 'revise & resubmit' decision at Rangeland Ecology and Management

---
+++ Code/rvzn-stats.R
@@ -0,0 +1,32 @@
+##  ------------------------------------------------------------------------------------------  ##
+                # Vegetation Response to Anti-Tall Fescue Herbicide Treatments
+##  ------------------------------------------------------------------------------------------  ##
+# Code written by Nicholas J Lyon
+
+# PURPOSE ####
+  ## The revise & resubmit review we got at Rangeland Ecology and Management requires a ∆ of stats
+  ## So, rather that changing the *many* tests we ran prior to this
+  ## I'm just going to write a totally new script for just the final version of this paper
+  ## If you're a reader of the paper, this includes 100% of the results in the publication
+
+# Set the working directory
+setwd("~/Documents/School/_Publications/2020_Coon_Daubenmire SnS/Daubenmire.HerbicideComponent.WD")
+  ## if you're not me you will need to re-set this to your own computer.
+
+# Clear the environment
+rm(list = ls())
+
+# Load required libraries
+library(lme4); library(lmerTest)
+
+## ----------------------------------- ##
+
+## ----------------------------------- ##
+
+
+
+
+
+
+
+# END ####
