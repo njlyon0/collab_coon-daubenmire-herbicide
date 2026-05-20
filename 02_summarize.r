d8dd4d4ca563a276bb01cb52e9cbdb58d5c3988b
njlyon0 <njlyon@alumni.iastate.edu>
2026-05-20 14:14:48 -0400

    feat (summarize): add draft summarization workflow

--- 02_summarize.r
+++ 02_summarize.r
@@ -20,8 +20,29 @@ sry_v01 <- read.csv(file =  file.path("data", "01_daub-tidy.csv"))
 # Check structure
 dplyr::glimpse(sry_v01)
 
+##  ------------------------------------------  ##
+# Prep the Data ----
+##  ------------------------------------------  ##
 
+# Reshape the data into long format
+sry_v02 <- sry_v01 %>% 
+  tidyr::pivot_longer(cols = -year:-patch,
+    names_to = "variable", values_to = "value")
 
+# Check structure
+dplyr::glimpse(sry_v02)
+
+##  ------------------------------------------  ##
+# Summarize the Data ----
+##  ------------------------------------------  ##
+
+# Calculate the summary of all variables
+sry_v03 <- supportR::summary_table(data = sry_v02,
+  groups = setdiff(names(sry_v02), "value"),
+  response = "value", drop_na = TRUE)
+
+# Check structure
+dplyr::glimpse(sry_v03)
 
 # End ----
 
