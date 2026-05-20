a5aef0f46d946a787391f362e870f18330216625
njlyon0 <njlyon@alumni.iastate.edu>
2026-05-20 11:54:04 -0400

    feat: add draft setup script

---
+++ -setup.r
@@ -0,0 +1,23 @@
+##  ------------------------------------------------------------  ##
+# Daubenmire Project - Setup
+##  ------------------------------------------------------------  ##
+# Purpose:
+## Do generally-useful setup tasks
+
+# Load needed libraries
+# install.packages("librarian")
+librarian::shelf(tidyverse)
+
+# Clear environment
+rm(list = ls()); gc()
+
+##  ------------------------------------------  ##
+# Make Folders ----
+##  ------------------------------------------  ##
+
+# Create needed folder(s)
+dir.create(path = file.path("data", "raw-data"),
+    showWarnings = FALSE, recursive = TRUE)
+dir.create(path = file.path("graphs"), showWarnings = FALSE)
+
+# End ----
