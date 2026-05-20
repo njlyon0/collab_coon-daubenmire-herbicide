d1730aaeae4d3724c5389b735d7326c800178f7e
njlyon0 <njlyon@alumni.iastate.edu>
2026-05-20 12:12:12 -0400

    feat (wrangle): add refined draft of script

---
+++ 01_wrangle.r
@@ -0,0 +1,195 @@
+##  ------------------------------------------------------------  ##
+# Daubenmire Project - Wrangle Data
+##  ------------------------------------------------------------  ##
+# Purpose:
+## Wrangle data (e.g., perform quality contorl, calculate needed metrics)
+
+# Load needed libraries
+# install.packages("librarian")
+librarian::shelf(tidyverse, supportR)
+
+# Get set up
+source("-setup.r")
+
+# Clear environment
+rm(list = ls()); gc()
+
+# Load data
+daub_v01 <- read.csv(file = file.path("data", "raw-data", "daubenmire-project_raw-data.csv"))
+
+# Check structure
+dplyr::glimpse(daub_v01)
+
+##  ------------------------------------------  ##
+# Fix Column Class Issues ----
+##  ------------------------------------------  ##
+
+# Reshape data to get all ostensibly numeric columns into one column
+daub_v02 <- daub_v01 %>% 
+  dplyr::mutate(row_id = 1:nrow(.), .before = dplyr::everything()) %>% 
+  dplyr::mutate(dplyr::across(.cols = dplyr::everything(),
+    .fns = as.character)) %>% 
+  tidyr::pivot_longer(cols = Robel.N:Angle_of_O,
+    names_to = "vars", values_to = "values")
+
+# Check for non-numeric characters
+supportR::num_check(data = daub_v02, col = "values")
+
+# Replace problem values and re-reshape back to original format
+daub_v03 <- daub_v02 %>% 
+  dplyr::mutate(values = ifelse(test = values %in% c("", " ", "."),
+    yes = NA, no = values)) %>% 
+  dplyr::mutate(values = as.numeric(values)) %>% 
+  tidyr::pivot_wider(names_from = vars, values_from = values)
+
+# Check structure
+dplyr::glimpse(daub_v03)
+
+##  ------------------------------------------  ##
+# Re-Calculate Robel Information ----
+##  ------------------------------------------  ##
+
+# Re-calculate average/std. dev of Robel
+robel_v01 <- daub_v03 %>% 
+  dplyr::select(-Avg_Robel, -SD_Robel) %>% 
+  tidyr::pivot_longer(cols = dplyr::starts_with("Robel.")) %>% 
+  dplyr::group_by(Pasture_Patch_Year_Transect) %>% 
+  dplyr::summarize(mean.robel_dm = mean(value, na.rm = TRUE),
+    std.dev.robel_dm = sd(value, na.rm = TRUE),
+    .groups = "drop") 
+
+# Check structure
+dplyr::glimpse(robel_v01)
+
+# Update Robel data in original dataset
+daub_v04 <- daub_v03 %>%
+  dplyr::select(-Avg_Robel, -SD_Robel) %>% 
+  dplyr::rename(robel.north_dm = Robel.N,
+    robel.east_dm = Robel.E,
+    robel.south_dm = Robel.S,
+    robel.west_dm = Robel.W) %>% 
+  dplyr::left_join(y = robel_v01, by = "Pasture_Patch_Year_Transect") %>% 
+  dplyr::relocate(dplyr::contains("robel"), .before = WSG)
+
+# Re-check structure
+dplyr::glimpse(daub_v04)
+
+##  ------------------------------------------  ##
+# Repair Daubenmire Categories ----
+##  ------------------------------------------  ##
+
+# Remaining vegetation categories were quantified as pseudo-categorical percents
+## Only allowed values are: 0, 1, 3, 16, 38, 63, 86, 98
+veg_v01 <- daub_v04 %>% 
+  dplyr::select(row_id, Pasture_Patch_Year_Transect, WSG:Litter, Seed_mix) %>% 
+  tidyr::pivot_longer(cols = -row_id:-Pasture_Patch_Year_Transect)
+
+# Check values
+sort(unique(veg_v01$value))
+
+# Repair back into allowed bins & make better column names
+veg_v02 <- veg_v01 %>% 
+  dplyr::mutate(value = dplyr::case_when(
+    value == 2 ~ 3,
+    value %in% c(6, 15, 165) ~ 16,
+    value %in% c(28, 36, 58) ~ 38,
+    value == 80 ~ 86,
+    value == 96 ~ 98,
+    TRUE ~ value)) %>% 
+  dplyr::mutate(name = dplyr::case_when(
+    name == "Bare" ~ "bare.ground_binned.perc",
+    name == "CSG" ~ "cool.season.grass_binned.perc",
+    name %in% c("Fescue", "Forbs", "Legumes", 
+        "Sedges") ~ paste0(tolower(name), "_binned.perc"),
+    name == "Seed_mix" ~ "seedmix.forbs_binned.perc.of.all.forbs",
+    name == "Litter" ~ "plant.litter_binned.perc",
+    name == "Violets" ~ "prairie.violets_binned.perc",
+    name == "Woody" ~ "woody.plants_binned.perc",
+    name == "WSG" ~ "warm.season.grass_binned.perc",
+    TRUE ~ name)) %>% 
+  tidyr::pivot_wider(names_from = name, values_from = value)
+
+# Check structure
+dplyr::glimpse(veg_v02)
+
+# Re-attach to broader daubenmire data
+daub_v05 <- daub_v04 %>% 
+  dplyr::select(-WSG:-Litter, -Seed_mix) %>% 
+  dplyr::left_join(y = veg_v02, by = c("row_id", "Pasture_Patch_Year_Transect"))
+
+# Re-check structure
+dplyr::glimpse(daub_v05)
+
+##  ------------------------------------------  ##      
+# Summarize within Patch ----
+##  ------------------------------------------  ##      
+
+# Summarize within patch (i.e., across quadrats from 2 transects / patch)
+daub_v06 <- daub_v05 %>% 
+  dplyr::select(-row_id, -Patch, -Pasture_Patch_Year,
+    -Pasture_Patch_Year_Transect, -Angle_of_O) %>% 
+  dplyr::rename(year = Year,
+    pasture = Pasture,
+    patch = Pasture_Patch,
+    litter.depth_cm = Litter_dep) %>% 
+  tidyr::pivot_longer(cols = robel.north_dm:seedmix.forbs_binned.perc.of.all.forbs) %>% 
+  dplyr::group_by(year, pasture, patch, name) %>% 
+  dplyr::summarize(value = mean(value, na.rm = TRUE),
+    .groups = "drop") %>% 
+  tidyr::pivot_wider()
+
+# Re-check structure
+dplyr::glimpse(daub_v06)
+
+##  ------------------------------------------  ##      
+# Clarify Panic Grass Data ----
+##  ------------------------------------------  ##      
+
+daub_v07 <- daub_v06 %>% 
+  dplyr::mutate(panic.grass_pres.abs = dplyr::case_when(
+    Panic > 0 ~ 1,
+    is.na(Panic) ~ NA, 
+    TRUE ~ Panic)) %>% 
+  dplyr::select(-Panic)
+
+# Check structure
+dplyr::glimpse(daub_v07)
+
+##  ------------------------------------------  ##      
+# Reorder Columns ----
+##  ------------------------------------------  ##      
+
+# Reorder remaining columns
+daub_v08 <- daub_v07 %>% 
+  dplyr::relocate(dplyr::starts_with("robel."), .after = patch) %>% 
+  dplyr::relocate(dplyr::contains(".robel"), .after = robel.west_dm) %>% 
+  dplyr::relocate(panic.grass_pres.abs, .after = std.dev.robel_dm) %>% 
+  dplyr::relocate(dplyr::starts_with("bare.ground"), dplyr::starts_with("plant.litter"),
+    dplyr::contains("season.grass"), dplyr::starts_with("fescue"),
+    dplyr::starts_with("sedges"), dplyr::starts_with("woody.plants"),
+    dplyr::starts_with("forbs"), dplyr::starts_with("seedmix"),
+    dplyr::starts_with("prairie.violets"), dplyr::starts_with("legumes"),
+    .after = panic.grass_pres.abs) %>% 
+  dplyr::relocate(litter.depth_cm, .after = std.dev.robel_dm)
+  
+# Make sure no columns are lost accidentally
+supportR::diff_check(old = names(daub_v07), new = names(daub_v08))
+
+# Re-check structure
+dplyr::glimpse(daub_v08)
+
+##  ------------------------------------------  ##      
+# Export ----
+##  ------------------------------------------  ##      
+
+# Make one final object
+daub_v99 <- daub_v08
+
+# Check structure
+dplyr::glimpse(daub_v99)
+
+# Export
+write.csv(x = daub_v99, row.names = FALSE, na = '',
+    file = file.path("data", "tidy-data", "01_daub-tidy.csv"))
+
+# End ----
